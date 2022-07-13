6a04d8dd282d8d4256a24d15936b6d91d2c53ffd
Sheikh Azizul Hakim <chemistry.fahim@gmail.com>
2022-07-14 05:52:43 +0600

    React responded to API

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+
+const restEndpoint = "/api/persons/1705002";
+
+const callRestApi = async () => {
+    const response = await fetch(restEndpoint, {method: "GET", headers: {'Accept': 'application/vnd.api+json'}});
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+    return JSON.stringify(jsonResponse);
+};
+
+function RenderResult() {
+    const [apiResponse, setApiResponse] = useState("*** now loading ***");
+
+    useEffect(() => {
+        callRestApi().then(
+            result => setApiResponse(result));
+    }, []);
+
+    return (
+        <div>
+            <h1>React App</h1>
+            <p>{apiResponse}</p>
+        </div>
+    );
+};
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <div>
+        <RenderResult />
+        <img src={"https:picsum.photos/500/300"} alt=""/></div>,
+    document.querySelector('#root')
 );
---
+++ run.py
@@ -0,0 +1,12 @@
+from app import app
+import flask_restless
+from models import *
+from qa_models import *
+
+
+# Create the Flask-Restless API manager.
+manager = flask_restless.APIManager(session=db.session)
+person_blueprint = manager.create_api(Person, methods=['GET', 'POST'])
+manager.init_app(app)
+
+app.run(debug=True)
