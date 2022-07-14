0c9e6d4f51d21d217c9b02b1aeb59cbb56bc5080
Saifur Rahman <srj.buet17@gmail.com>
2022-07-14 19:27:20 +0600

    Docker Added

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ manager = flask_restless.APIManager(session=db.session)
 person_blueprint = manager.create_api(Person, methods=['GET', 'POST'])
 manager.init_app(app)
 
-app.run(debug=True)
+app.run(debug=True,host='0.0.0.0')
